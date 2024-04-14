988faa95e7b5295f0e5e0e88ce32c1f43c4260d1
ccgargantua <lovelygargantua@gmail.com>
2024-04-14 15:18:56 -0400

    better name for alignof

--- garutil/headeronly/compiler.h
+++ garutil/headeronly/compiler.h
@@ -9,7 +9,7 @@
 
 
 #if __STDC_VERSION__ <= 201112L
-    #define alignof(type) offsetof(struct { char c; type d; }, d)
+    #define _Alignof(type) offsetof(struct { char c; type d; }, d)
     #define _Noreturn
 #endif
 
