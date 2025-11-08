9f073b3082808a7b4a998e587b27ce07d74f207d
ccgargantua <ccgargantua@proton.me>
2025-11-08 15:28:25 -0500

    debug now allows you to set alternative files, with the default being stderr.

--- util/debug/debug.c
+++ util/debug/debug.c
@@ -5,17 +5,25 @@
 
 #ifndef NDEBUG
 
-void dbg_printf(const char *fmt, ...)
+static FILE *log_file = NULL;
+
+void dbg_set_log_file(FILE *fp)
 {
+    log_file = fp;
+}
+
+void dbg_log(const char *fmt, ...)
+{
+    if (!log_file) log_file = stderr;
     va_list args;
     va_start(args, fmt);
-    vfprintf(stderr, fmt, args);
+    vfprintf(log_file, fmt, args);
     va_end(args);
 }
 
 #else
 
-void dbg_printf(const char *fmt, ...)
-{}
+void dbg_set_log_file(FILE *fp) {}
+void dbg_printf(const char *fmt, ...) {}
 
 #endif
--- util/debug/debug.h
+++ util/debug/debug.h
@@ -1,6 +1,9 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-void dbg_printf(const char *fmt, ...);
+#include <stdio.h>
+
+void dbg_set_log_file(FILE *fp);
+void dbg_log(const char *fmt, ...);
 
 #endif //DEBUG_H
